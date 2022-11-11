704dad13dafac77b93bbeef708c1c6dba246cc95
Scott Stauffer <sstauffer@amshot.com>
2022-11-11 15:03:34 -0600

    Checking in example script.

--- Metal.Core.CLI/Program.cs
+++ Metal.Core.CLI/Program.cs
@@ -1,40 +1,28 @@
 ﻿namespace Metal.Core.CLI
 {
-    using Metal.Core.Objects;
     using Metal.Core.Parsing;
-    using System.Text;
 
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            Module mod_MetalcoreLib = new ("MetalcoreLib");
-            Class o_StringContainer = new ("StringContainer", mod_MetalcoreLib);
-            Method m_GetSize = new ("GetSize", o_StringContainer, Typing.Enums.ValueType.Number);
+            ParserResult result = new ();
+            
+            if (args.Length > 0)
+            {
+                var script = args[0];
 
-            StringBuilder script = new ();
+                if (System.IO.File.Exists(script) && script.TrimEnd('"').EndsWith(".metal"))
+                {
+                    result = Parser.ParseScript(script);
+                }
+                else
+                {
+                    result = Parser.Parse(script);
+                }
+            }
 
-            script.AppendLine("# an example script                     ");
-            script.AppendLine("class Person                            ");
-            script.AppendLine("  method greet(name = \"Scott\")        ");
-            script.AppendLine("    println \"Hello! My name is {name}\"");
-            script.AppendLine("  end                                   ");
-            script.AppendLine("end                                     ");
-            script.AppendLine("                                        ");
-            script.AppendLine("class Developer < Person                ");
-            script.AppendLine("  method writeCode()                    ");
-            script.AppendLine("    println \"I am writing code.\"      ");
-            script.AppendLine("  end                                   ");
-            script.AppendLine("end                                     ");
-            script.AppendLine("                                        ");
-            script.AppendLine("# instantiate new Person instance       ");
-            script.AppendLine("@p = Person.new()                       ");
-            script.AppendLine("@p.greet(\"Scott Christopher Stauffer\")");
-
-            ParserResult result = Parsing.Parser.Parse(script.ToString());
-
-            Console.WriteLine("Press enter to close.");
-            Console.ReadLine();
+            return result.Success ? 0 : 1;
         }
     }
 }
