cff8894f2320c847887c93dd93ec734edf212640
Scott Stauffer <sstauffer@amshot.com>
2022-11-11 16:55:01 -0600

    Implemented REPL parsing.

--- Metal.Core.CLI/Program.cs
+++ Metal.Core.CLI/Program.cs
@@ -6,8 +6,13 @@
     {
         public static int Main(string[] args)
         {
+            return args.Length > 0 ? ConsoleMode(args) : ReplMode();
+        }
+
+        private static int ConsoleMode(string[] args)
+        {
             ParserResult result = new ();
-            
+
             if (args.Length > 0)
             {
                 var script = args[0];
@@ -23,6 +28,30 @@
             }
 
             return result.Success ? 0 : 1;
+        }
+
+        private static int ReplMode()
+        {
+            Parser.ReplEnabled = true;
+            int result = 0;
+            bool running = true;
+
+            while (running)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+
+                if (line.Trim().ToLower() == "exit")
+                {
+                    running = false;
+                }
+                else
+                {
+                    Parser.ReplParse(line);
+                }
+            }
+
+            return result;
         }
     }
 }
